c5d80d0e703625bf4bedcc7e87ab3807c4d56b0f
Zeltinbit <Tinbit23@gmail.com>
2026-07-18 22:36:18 +0300

    Start React portfolio setup with reusable components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+
+function App() {
+
+    return (
+
+        <>
+            <Navbar />
+            <Hero />
+            <About />
+            <h1>Hello, I'm Tinbite Daniel</h1>
+        </>
+
+    );
+
+}
+
+export default App;
